5bcc86d268378ec117244657a782959ab4869c19
chrisweb <chris963@gmail.com>
2024-10-26 19:04:36 +0200

    explicitly enable react strict mode and disable nextjs powered by header

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,24 @@
 import type { NextConfig } from 'next'
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
 
-const nextConfig: NextConfig = {
-    experimental: {
-        // experimental typescript "statically typed links"
-        // https://nextjs.org/docs/app/api-reference/next-config-js/typedRoutes
-        typedRoutes: true,
+const nextConfig = (phase: string) => {
+
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        console.log('happy development session ;)')
     }
-};
+
+    const nextConfigOptions: NextConfig = {
+        reactStrictMode: true,
+        poweredByHeader: false,
+        experimental: {
+            // experimental typescript "statically typed links"
+            // https://nextjs.org/docs/app/api-reference/next-config-js/typedRoutes
+            typedRoutes: true,
+        }
+    };
+
+    return nextConfigOptions
+
+}
 
 export default nextConfig
