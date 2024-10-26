a508a6b28e163ef8ad553be8094e44b685af47bc
chrisweb <chris963@gmail.com>
2024-10-26 18:20:02 +0200

    typed routes option enabled

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        // experimental typescript "statically typed links"
+        // https://nextjs.org/docs/app/api-reference/next-config-js/typedRoutes
+        typedRoutes: true,
+    }
 };
 
-export default nextConfig;
+export default nextConfig
