3c3416c0bca00bb6d84c60274062a7681b5e2011
chrisweb <chris963@gmail.com>
2024-10-30 19:50:09 +0100

    fresh sentry setup, trying wizard again

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from 'next'
 import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
-import { withSentryConfig } from '@sentry/nextjs'
 
 const nextConfig = (phase: string) => {
 
@@ -22,39 +21,4 @@ const nextConfig = (phase: string) => {
 
 }
 
-const nextConfigWithSentry = withSentryConfig(
-    nextConfig,
-    {
-        // For all available options, see:
-        // https://github.com/getsentry/sentry-webpack-plugin#options
-
-        // Only print logs when uploading source maps during CI
-        silent: !process.env.CI,
-
-        org: "MY_ORGANIZATION_NAME",
-        project: "MY_PROJECT_NAME",
-
-        // For all available options, see:
-        // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-        // Upload a larger set of source maps for prettier stack traces (increases build time)
-        widenClientFileUpload: true,
-
-        // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-        tunnelRoute: "/monitoring",
-
-        // Hides source maps from generated client bundles
-        hideSourceMaps: true,
-
-        // Automatically tree-shake Sentry logger statements to reduce bundle size
-        disableLogger: true,
-
-        // Enables automatic instrumentation of Vercel Cron Monitors.
-        // See the following for more information:
-        // https://docs.sentry.io/product/crons/
-        // https://vercel.com/docs/cron-jobs
-        automaticVercelMonitors: true,
-    }
-)
-
-export default nextConfigWithSentry
+export default nextConfig
