5cc7f56c8390746e81b6be98aa6e8165017f5c72
chrisweb <chris963@gmail.com>
2024-11-03 18:27:37 +0100

    replace classic typescript linting with flat config version

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,8 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
+import tseslint from 'typescript-eslint'
+import type { FlatConfig } from '@typescript-eslint/utils/ts-eslint'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -11,19 +13,44 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 })
 
+const tsESLintConfig = tseslint.config(
+    // eslint recommended rules
+    js.configs.recommended,
+    //...tseslint.configs.recommended,
+    // OR more type checked rules
+    //...tseslint.configs.recommendedTypeChecked,
+    // OR more strict rules
+    ...tseslint.configs.strictTypeChecked,
+    // optional stylistic rules
+    //...tseslint.configs.stylistic
+    // OR the type checked version
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        }
+    },
+    {
+        files: ['**/*.ts', '**/*.tsx']
+    }
+)
+
 export default [
-    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    ...compat.extends('next/core-web-vitals'),
     {
         ignores: [
-            '**/node_modules/',
-            '**/.next/',
-            '**/.vscode/',
-            '**/public/',
-            '**/LICENSE',
-            'tests/eslint/',
-        ],
+            '.next/',
+            '.vscode/',
+            'public/',
+        ]
+    },
+    {
         linterOptions: {
             reportUnusedDisableDirectives: 'warn'
-        },
-    }
-]
+        }
+    },
+    ...tsESLintConfig,
+] satisfies FlatConfig.Config[]
