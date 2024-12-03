ec4798f912d78635b3a3abec678bb71dcab774f2
chrisweb <chris963@gmail.com>
2024-12-03 17:07:28 +0100

    first example, styling using css modules

---
+++ components/tutorial_examples/UI/Button.tsx
@@ -0,0 +1,12 @@
+import styles from './button.module.css'
+
+const UIButton: React.FC = () => {
+
+    return (
+        <button className={styles.reset}>
+            I&apos;m a UI button
+        </button>
+    )
+}
+
+export default UIButton
