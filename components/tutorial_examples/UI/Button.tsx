feb5053c7cface63eed6dccbd19c1b80dada2eaa
chrisweb <chris963@gmail.com>
2024-12-03 17:09:13 +0100

    second example

--- components/tutorial_examples/UI/Button.tsx
+++ components/tutorial_examples/UI/Button.tsx
@@ -3,7 +3,7 @@ import styles from './button.module.css'
 const UIButton: React.FC = () => {
 
     return (
-        <button className={styles.reset}>
+        <button className={`${styles.reset} ${styles.core}`}>
             I&apos;m a UI button
         </button>
     )
