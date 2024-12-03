1fc9700a4300728f7c2e8232cd7db655315c7027
chrisweb <chris963@gmail.com>
2024-12-03 19:53:17 +0100

    add device sizes for next/image

--- next.config.ts
+++ next.config.ts
@@ -49,6 +49,7 @@ const nextConfig = (phase: string) => {
         images: {
             // file formats for next/image
             formats: ['image/avif', 'image/webp'],
+            deviceSizes: [384, 640, 750, 828, 1080, 1200, 1920, 2176, 3840],
         },
     }
 
