1d0f944fabe17e8fd33ec6dddb244a7c77e014d1
chrisweb <chris963@gmail.com>
2024-12-03 18:53:08 +0100

    adding a navigation to the layout

---
+++ components/header/Navigation.tsx
@@ -0,0 +1,34 @@
+import type { Route } from 'next'
+
+interface IMainMenuItem {
+    pathname: string
+    text: string
+}
+
+const HeaderNavigation: React.FC = () => {
+
+    const mainMenuItems: IMainMenuItem[] = [
+        { pathname: '/', text: 'Home' },
+        { pathname: '/blog', text: 'Blog' },
+    ]
+
+    return (
+        <>
+            <nav id="navigation">
+                {mainMenuItems.map((menuItem) => {
+                    return (
+                        <a
+                            href={menuItem.pathname as Route}
+                            key={menuItem.pathname}
+                            title={menuItem.text}
+                        >
+                            {menuItem.text}
+                        </a>
+                    )
+                })}
+            </nav>
+        </>
+    )
+}
+
+export default HeaderNavigation
