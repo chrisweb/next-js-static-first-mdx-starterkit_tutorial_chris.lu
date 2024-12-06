37ea81fa3e3b0501f8e2397adcf20341cacfd99c
chrisweb <chris963@gmail.com>
2024-12-06 19:03:57 +0100

    shared metadata

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,14 @@
+import type { Metadata } from 'next'
+import { sharedMetadata } from '@/shared/metadata'
+
+export const metadata: Metadata = {
+    title: 'Blog page',
+    openGraph: {
+        ...sharedMetadata.openGraph,
+        url: 'https://example.com/blog'
+    }
+}
+
 export default function Blog() {
 
     return (
