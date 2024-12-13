fa31b0192bb9d2b961400b66c7c3324ed4a5e443
chrisweb <chris963@gmail.com>
2024-12-13 14:03:54 +0100

    adjust types as page params are now async

--- app/blog/posts/[title]/page.tsx
+++ app/blog/posts/[title]/page.tsx
@@ -1,14 +1,21 @@
 import { sharedMetadata } from '@/shared/metadata'
+import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
+
+interface BlogPostsProps {
+    params: Promise<{ title: string }>
+}
 
 const titlesAllowList = ['foo'] as string[]
 
-export function generateMetadata({ params }: BlogPostsProps) {
+export async function generateMetadata({ params }: BlogPostsProps): Promise<Metadata> {
 
     let title = '' as string
 
-    if (titlesAllowList.includes(params.title)) {
-        title = params.title
+    const pageParams = await params
+
+    if (titlesAllowList.includes(pageParams.title)) {
+        title = pageParams.title
     }
 
     return {
@@ -16,25 +23,22 @@ export function generateMetadata({ params }: BlogPostsProps) {
         openGraph: {
             ...sharedMetadata.openGraph,
             url: `https://example.com/blog/posts/${title}`,
-        }
+        },
     }
 
 }
 
-interface BlogPostsProps {
-    params: {
-        title: string
-    }
-}
-
-export default function Blog({ params }: BlogPostsProps) {
-
+export default async function Blog({
+    params,
+}: BlogPostsProps) {
     // as we are getting user data we need 
     // to sanitize it or use an allow list
     let title = '' as string
 
-    if (titlesAllowList.includes(params.title)) {
-        title = params.title
+    const pageParams = await params
+
+    if (titlesAllowList.includes(pageParams.title)) {
+        title = pageParams.title
     } else {
         notFound()
     }
